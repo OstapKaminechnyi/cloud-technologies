db49dfa107dfa0249270d6023c0b8ddf7490e1a8
OstapKaminechnyi <ostap1599@gmail.com>
2019-10-27 15:24:21 +0200

    add rest controller

---
+++ src/main/java/com/retail/repos/PanelRepo.java
@@ -0,0 +1,8 @@
+package com.retail.repos;
+
+import com.retail.domain.Panel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PanelRepo extends CrudRepository<Panel,Integer > {
+
+}
